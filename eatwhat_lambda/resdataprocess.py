fbd3c4ac65730748a17fd7a8f6eec385d7034bd5
lechchu <chien501225@gmail.com>
2021-01-11 03:42:47 +0800

    fix first use.

--- eatwhat_lambda/resdataprocess.py
+++ eatwhat_lambda/resdataprocess.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import random
+import urllib
 # from pandas import DataFrame
 from boto3.dynamodb.conditions import Key, Attr
 
@@ -9,36 +10,75 @@ class RestaurantData:
     def databaseConnect(self):
 
         self.dynamodb = boto3.resource('dynamodb')
-        self.table = self.dynamodb.Table('linebot_EATWhat_DB')
+        self.restable = self.dynamodb.Table('linebot_EATWhat_DB')
+        self.usertable = self.dynamodb.Table('linebot_EATWhat_Users')
 
 
 
     def makeResFlex(self, resdatas):
         reslist = json.load(open('flexJson/restaurant_emptylist.json',"r",encoding="utf-8"))
         for resdata in resdatas:
-       
+            # print(resdata.iloc[i]['name'])          
             json_temp = json.load(open('flexJson/restaurant_template.json',"r",encoding="utf-8"))
+            # return(json_temp)
             json_temp['hero']['url'] = resdata['resImage'] #res pic url
             json_temp['body']['contents'][0]['text']= resdata['resName'] #res name
             json_temp['body']['contents'][2]['contents'][1]['contents'][1]['text'] = resdata['resAddress'] #res add
             res_add = json_temp['body']['contents'][2]['contents'][1]['contents'][1]['text']
-            json_temp['body']['contents'][2]['contents'][1]['contents'][1]['action']['uri']+=res_add #res add uri
+            json_temp['body']['contents'][2]['contents'][1]['contents'][1]['action']['uri']+=(urllib.parse.quote(res_add)+'/') #res add uri
             json_temp['body']['contents'][2]['contents'][0]['contents'][1]['text'] = resdata['resType'] #res type      
 
             reslist['contents'].append(json_temp)
 
         return reslist
 
-    def getFlexByFilter(self, keyword, header='resType'):
+    def getFlexByFilter(self, keyword, userid, header='resType'):
 
-        response = self.table.scan(
-            FilterExpression=Attr(header).eq(keyword)
-            # , Limit=4
+        self.updateUserResList(userid)
+        temp = []
+        for res in self.res_list:
+            if res['resType'] == keyword:
+                temp.append(res)
+
+        count = 3
+        if len(temp)<3:
+            count = len(temp)
+        return self.makeResFlex(random.sample(temp, count))
+
+
+    def updateUserResList(self, userId):
+        self.user_reslist = self.usertable.query(
+        KeyConditionExpression=Key('userId').eq(userId)
         )
-        return self.makeResFlex(random.sample(response['Items'], 3))
+
+        #if user not in database, init the data 
+        if self.user_reslist['Count'] == 0:
+            user_reslist = []
+            for i in range(len(self.res_list)):             
+                user_reslist.append(int(self.res_list[i]['resID']))
+
+            self.usertable.put_item(       
+                Item={
+                'userId': userId,
+                'resList': user_reslist
+                }
+            )
+
+        # make reslist to user own reslist
+        self.user_reslist = self.user_reslist['Items'][0]['resList']
+
+        temp =[]
+        for i in range(len(self.res_list)):
+            if int(self.res_list[i]['resID']) in self.user_reslist:               
+                temp.append( self.res_list[i])
+        self.res_list = temp
+
+        
 
     def __init__(self):
         self.databaseConnect()
+        self.res_list = self.restable.scan()['Items']
+        self.user_reslist = self.res_list
         self.res_template = json.load(open('./flexJson/restaurant_template.json',"r",encoding="utf-8"))
         self.res_type = json.load(open('./flexJson/restaurant_type.json',"r",encoding="utf-8"))
     
