73ed018f525c49ab8f03638d42333a0caef3a926
lechchu <chien501225@gmail.com>
2021-01-11 23:57:00 +0800

    try fix update db on iphone, add alert

--- js/liff.js
+++ js/liff.js
@@ -108,15 +108,16 @@ window.addEventListener('load', () => {
         ReturnValues:"UPDATED_NEW"
     };
     try{
-    docClient.update(params, function(err, data) {
+    const update_result = docClient.update(params, function(err, data) {
         if (err) {
             console.log("Unable to update item: " + "\n" + JSON.stringify(err, undefined, 2));
             // alert(JSON.stringify(err, undefined, 2));
         } else {
             console.log("UpdateItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
-            // alert(JSON.stringify(data, undefined, 2));
+            alert(JSON.stringify(data, undefined, 2));
         }
     });
+    alert(update_result);
   }catch(e){
     alert(e);
   }
